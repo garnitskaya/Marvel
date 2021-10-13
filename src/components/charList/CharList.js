7bdb2917c1c30105f4a3372f2a66f0970eb7b5e3
Alena <9972211@gmail.com>
2021-10-13 16:16:36 +0300

    ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 
 import MarvelService from './../../services/MarvelService';
@@ -58,8 +58,21 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+
     rendetItem(arr) {
-        const items = arr.map(item => {
+        const items = arr.map((item, i) => {
             const { id, name, thumbnail } = item;
             let imgStyle = { 'objectFit': 'cover' };
             if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
@@ -69,8 +82,19 @@ class CharList extends Component {
             return (
                 <li
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={id}
-                    onClick={() => this.props.onCharSelected(id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === '' || e.key === 'Enter') {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                     <img src={thumbnail} alt="abyss" style={imgStyle} />
                     <div className="char__name">{name}</div>
                 </li>
@@ -110,7 +134,7 @@ class CharList extends Component {
 }
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
