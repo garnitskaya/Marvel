54030c1ad8b27a851d0083c0808dac933f40e467
Alena <9972211@gmail.com>
2021-11-23 14:30:37 +0200

    React-helmet

--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import './singleCharacterLayout.scss';
 
@@ -7,6 +8,12 @@ const SingleCharacterLayout = ({ data }) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${name} page`} />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__img" />
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
