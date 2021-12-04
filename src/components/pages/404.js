2f30893f7d2200651e0773320a27038f0f00951e
Alena <9972211@gmail.com>
2021-12-04 21:58:43 +0200

    Updates

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,24 +1,16 @@
 import ErrorMessage from "../errorMessage/ErrorMessage"
-import { useHistory } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
-    const history = useHistory();
-
-    const goBack = () => {
-        history.goBack();
-    }
-
     return (
         <div>
             <ErrorMessage />
             <p style={{ 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px' }}>Page doesn't exist</p>
-            <button
-                onClick={goBack}
+            <Link to='/'
                 style={{
                     'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px',
                     'margin': '30px auto', 'background': 'none', 'border': 'none', 'cursor': 'pointer'
-                }}
-                to='/'>Back to main page</button>
+                }}>Back to main page</Link>
         </div >
     )
 }
